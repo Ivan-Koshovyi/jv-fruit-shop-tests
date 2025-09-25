8981625c15166676c326062e861bcef8a3ce6188
ivankosh <ivan2023koshovyi@gmail.com>
2025-09-25 20:56:35 +0300

    check-style

--- src/test/java/core/basesyntax/service/impl/ReadFileImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReadFileImplTest.java
@@ -2,13 +2,7 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.service.ReadFile;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
